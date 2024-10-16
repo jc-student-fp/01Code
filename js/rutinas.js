8a353ab55f3b01e48548a0201ef0552f0814219d
jc-student-fp <jgarrido605r@fpdrioja.com>
2024-10-16 17:46:28 +0200

    inicio

---
+++ js/rutinas.js
@@ -0,0 +1,11 @@
+function fijarHora() {
+    const fecha = new Date();    
+    let cadena = fecha.getHours() + ':' + fecha.getMinutes() + ':' + fecha.getSeconds();
+        
+    document.getElementById('hora').innerText = cadena;        
+}
+
+window.addEventListener(
+    'load',
+    setInterval(() => fijarHora(), 1000),
+    false);
